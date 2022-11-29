1ff663a311393c93a5b7cf65cf408217ef01d808
delarosaii17 <116123382+delarosaii17@users.noreply.github.com>
2022-11-29 15:24:39 -0800

    Completed Blocking Movement and Health Collectables.
    I completed the Blocking Movement and Health Collectable tutorials in Rubys Adventure.

--- RubysAdventureDeLaRosaP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureDeLaRosaP7/Assets/Scripts/RubyController.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth; 
+
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical; 
+
     // Start is called before the first frame update
     void Start()
     {
-       //Chnages Frame Rate
-       //Qualitysettings.vsynccount = 0;
-       //Application.targetFrameRate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical"); 
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical"); 
+    }
+
+    void FixedUpdate()
+    {
         Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position; 
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
