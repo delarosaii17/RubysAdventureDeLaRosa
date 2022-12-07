979dd8ebd46366017b8301d0231c42a001e593e5
delarosaii17 <116123382+delarosaii17@users.noreply.github.com>
2022-12-07 15:23:16 -0800

    Finished Animations and Started Projectiles
    I tried to edit things but like they didnt work and I am very upset and waaaaa

--- RubysAdventureDeLaRosaP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureDeLaRosaP7/Assets/Scripts/RubyController.cs
@@ -9,6 +9,8 @@ public class RubyController : MonoBehaviour
     public int maxHealth = 5;
     public float timeInvincible = 2.0f;
 
+    public GameObject projectilePrefab;
+
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -17,12 +19,17 @@ public class RubyController : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
     float horizontal;
-    float vertical; 
+    float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
         currentHealth = maxHealth;
     }
 
@@ -32,11 +39,28 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0)
                 isInvincible = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
         }
     }
 
@@ -53,6 +77,10 @@ public class RubyController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
+            if (isInvincible)
+                return;
+
             if (isInvincible)
                 return;
 
@@ -61,5 +89,15 @@ public class RubyController : MonoBehaviour
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
